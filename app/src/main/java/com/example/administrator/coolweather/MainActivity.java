32b0fc703adaee745cc710dfe27807da5d1864d9
lyy <756669461@qq.com>
2017-05-05 10:15:02 +0800

    初次提交

--- app/src/main/java/com/example/administrator/coolweather/MainActivity.java
+++ app/src/main/java/com/example/administrator/coolweather/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.administrator.coolweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.administrator.coolweather.db.Province;
 
 public class MainActivity extends AppCompatActivity {
+
+       private String[] data={"北京市","浙江省","江西省","四川省","江苏省"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listView= (ListView) findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,data);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                Toast.makeText(MainActivity.this,"",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
     }
 }
