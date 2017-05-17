da05fe55393fa99c016792c2330cf7c78cb3390f
lyy <756669461@qq.com>
2017-05-17 18:37:50 +0800

    第4次提交  使用selectedProvinceIndex

--- app/src/main/java/com/example/administrator/coolweather/MainActivity.java
+++ app/src/main/java/com/example/administrator/coolweather/MainActivity.java
@@ -18,45 +18,85 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+       private  List<String> dataList=new ArrayList<>();
+     // private  List<String> dataList1=new ArrayList<>();
 
-       private String[] provinces={"北京", "浙江省", "江西省", "四川省", "江苏省", "重庆市", "河北省", "山西省", "辽宁省", "吉林省", "黑龙江省"};
-       private String[] city01 = {"北京"};
-       private String[] city17 ={"杭州", "温州", "绍兴", "湖州", "衢州", "金华", "台州", "嘉兴", "舟山", "宁波", "丽水"};
+       private List<String>  provinces=Arrays.asList(new String[]{"北京", "浙江省", "江西省", "四川省", "江苏省", "重庆市", "河北省", "山西省", "辽宁省", "吉林省", "黑龙江省"});
+       private List<String>  city01 = Arrays.asList(new String[]{"北京"});
+       private List<String>  city02 =Arrays.asList(new String[]{"杭州", "温州", "绍兴", "湖州", "衢州", "金华", "台州", "嘉兴", "舟山", "宁波", "丽水"});
+      // private List<String>  city = Arrays.asList(new String[]{"北京","杭州", "温州", "绍兴", "湖州", "衢州", "金华", "台州", "嘉兴", "舟山", "宁波", "丽水"});
+      // private List<String>  country1= Arrays.asList(new String[]{"下沙区", "西湖区","鹿城"});
+       private List<String>  country02 = Arrays.asList(new String[]{"瓯海", "永嘉","鹿城"});
+
+       private  int level=1;
+       private Button btn1=null;
+       private ListView listView=null;
+       private ArrayAdapter<String> adapter;
+       private int selectedProvinceIndex=0;
+
+
+
+    private void showCities(int position) {
+         level=2;
+         this.btn1.setVisibility(View.VISIBLE);
+         this.dataList.clear();
+         if("北京".equals(provinces.get(position))) {
+             this.dataList.addAll(city01);
+        }
+        else
+        {
+            this.dataList.addAll(city02);
+        }
+        this.adapter.notifyDataSetChanged();
+    }
+
+    private void showCounties(int position) {
+        level=3;
+        this.dataList.clear();
+        this.dataList.addAll(country02);
+        adapter.notifyDataSetChanged();
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button btn1= (Button) findViewById(R.id.button);
-         btn1.setVisibility(View.GONE);
-        final ListView listView= (ListView) findViewById(R.id.list_view);
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,provinces);
+        this.btn1= (Button) findViewById(R.id.button);
+        listView= (ListView) findViewById(R.id.ListView);
+        this.dataList.addAll(this.provinces);
+       // this.dataList1.addAll(this.city);
+        adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,this.dataList);
+        //adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,this.dataList1);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-         @Override
-         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-             btn1.setVisibility(View.VISIBLE);
-             btn1.setOnClickListener(new View.OnClickListener() {
-                 @Override
-                 public void onClick(View v) {
-                     ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,provinces);
-                     listView.setAdapter(adapter);
-                     btn1.setVisibility(View.GONE);
-                 }
-             });
-             Toast.makeText(MainActivity.this,provinces[position], Toast.LENGTH_SHORT).show();
-             String[] data=null;
-             if("北京".equals(provinces[position])) {
-                 data = city01;
-             }
-              else
-             {
-                 data=city17;
-             }
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (level == 1) {
+                    selectedProvinceIndex=position;
+                    showCities(selectedProvinceIndex);
 
-             ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,data);
-             listView.setAdapter(adapter);
-         }
-     });
+                } else if (level == 2) {
+                    showCounties(position);
+                } else {
+
+                }
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (level == 3) {
+                    showCities(MainActivity.this.selectedProvinceIndex);
+                } else if (level == 2) {
+                    level = 1;
+                    btn1.setVisibility(View.GONE);
+                    MainActivity.this.dataList.clear();
+                    MainActivity.this.dataList.addAll(provinces);
+                    MainActivity.this.adapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 }
